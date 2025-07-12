09c3338451be4982aa66438c9f5a6b2dc9a44dc1
CuriousJay <vijay.gopinathan011@gmail.com>
2025-07-12 09:46:15 +0530

    3/7/25 Cube

---
+++ Day 14 Activity 3.py
@@ -0,0 +1,12 @@
+def cube(a):
+    cubing = a*a*a
+    return cubing
+
+result = cube(3)
+print("the cube is : ", result)
+result2 = cube(5)
+print("the cube is : ", result2)
+result3 = cube(7)
+print("the cube is : ", result3)
+result4 = cube(10)
+print("the cube is : ", result4)
