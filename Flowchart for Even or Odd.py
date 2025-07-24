80335001639f3226523076ededf7f4ae4628d3ca
CuriousJay <vijay.gopinathan011@gmail.com>
2025-07-24 20:36:01 +0530

    24/7/25 Extra

---
+++ Flowchart for Even or Odd.py
@@ -0,0 +1,17 @@
+Start
+  |
+  v
+Enter a number
+  |
+  v
+Is number % 2 == 0?
+  |         |
+ Yes       No
+  |         |
+  v         v
+Print      Print
+"Even"    "Odd"
+  |         |
+  \_______ /
+      |
+     End
