fd4545dc3b9e01ce02b6a0c91e5ae08dbc28929e
CuriousJay <vijay.gopinathan011@gmail.com>
2025-07-29 19:22:07 +0530

    29/7/25

---
+++ Day 19 Activity 1.py
@@ -0,0 +1,16 @@
+empty_list = []
+print(empty_list)
+
+numbers = [1,2,3,4,5,]
+print(numbers)
+
+r = numbers[::-1]
+print(r)
+
+triples = [1,2,3]*3
+print(triples)
+
+a = 'elephant'
+ra = a[::-1]
+print(ra)
+
