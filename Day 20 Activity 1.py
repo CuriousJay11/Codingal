4fef16b94da1d95a20d75018d45620eee830f62f
CuriousJay <vijay.gopinathan011@gmail.com>
2025-08-19 19:45:06 +0530

    19/8/25

---
+++ Day 20 Activity 1.py
@@ -0,0 +1,13 @@
+my_tuple = (3, 4, 5, 6)
+tuple2 = ("a", "b", "c")
+n = len(my_tuple)
+
+print(my_tuple[0])
+print(my_tuple[-1])
+print("length of the tuple is ", n) 
+print(my_tuple + tuple2)
+
+nest_tuple = (1, 2, 3, (7, 8))
+print(nest_tuple[3][0])
+print(nest_tuple[3][1])
+print(my_tuple[-1])
